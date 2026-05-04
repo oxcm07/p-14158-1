43537de1e5899ea5983bc31c5c529ff9924de6fc
oxcm07 <oxcm0727@gmail.com>
2026-05-04 11:50:22 +0900

    7강

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
