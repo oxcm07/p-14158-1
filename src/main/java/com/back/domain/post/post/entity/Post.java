d064852c0fe5512e1faf4b58d5af86bedebd95f8
oxcm07 <oxcm0727@gmail.com>
2026-05-04 15:09:43 +0900

    020

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,20 +1,31 @@
 package com.back.domain.post.post.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
+@ToString
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private int id;
-    private String title;
-    private String content;
+    private final String title;
+    @Column(columnDefinition = "TEXT")
+    private final String content;
+
+    public Post() {
+        this.title = "";
+        this.content = "";
+    }
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -7,6 +7,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration
 public class BaseInitData {
     @Autowired
@@ -15,19 +17,25 @@ public class BaseInitData {
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            if (postRepository.count() > 0) return;
-
-            Post post1 = new Post();
-            post1.setTitle("제목 1");
-
-            postRepository.save(post1);
-
-            Post post2 = new Post();
-            post2.setTitle("제목 2");
-
-            postRepository.save(post2);
-
-            System.out.println("기본 데이터가 초기화되었습니다.");
+            work1();
+            work2();
         };
+    }
+
+    void work1() {
+        if (postRepository.count() > 0) return;
+
+        Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
+        Post post2 = postRepository.save(new Post("제목 1", "내용 2"));
+
+        System.out.println("기본 데이터가 초기화되었습니다.");
+    }
+
+    void work2() {
+        Optional<Post> opPost1 = postRepository.findById(1);
+        // SELECT * FROM post WHERE id = 1;
+        Post post1 = opPost1.get();
+
+        System.out.println("post1 : " + post1);
     }
 }
